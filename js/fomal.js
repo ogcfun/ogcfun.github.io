109ceb8b896164d4dd5b6f4486616be7d7681616
YLN-deng <yln_deng@163.com>
2024-11-06 23:59:03 +0800

    Site updated: 2024-11-06 23:58:55

--- js/fomal.js
+++ js/fomal.js
@@ -1319,11 +1319,11 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
   if (document.hidden) {
     //离开当前页面时标签显示内容
-    document.title = '👀跑哪里去了~';
+    document.title = 'Σ( ° △ °|||)︴不要离开我';
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = '🐖抓到你啦～';
+    document.title = 'ξ( ✿＞◡❛) 终于等到你';
     //两秒后变回正常标题
     titleTime = setTimeout(function () {
       document.title = OriginTitile;
